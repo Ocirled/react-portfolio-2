7150fce3cebadfcb397a273bc0026f99ca0ffd8a
Ocirled <delricolie111@gmail.com>
2025-09-16 09:23:24 +0700

    Updates

--- src/components/Elements/LinkIcon.jsx
+++
@@ -1,9 +0,0 @@
-const IconLink = ({ link, ri, type = 'fill', classname }) => {
-  return (
-    <a href={`${link}`} target='__blank'>
-      <i className={`ri-${ri}-fill ${classname}`}></i>
-    </a>
-  )
-}
-
-export default IconLink
